aad26e2ceaac474a150282617248267f438b5a0b
KajetanTomusiak <120165385+KajetanTomusiak@users.noreply.github.com>
2023-04-27 13:41:56 +0200

    importing db

--- db.sql
+++ db.sql
@@ -1,18 +1,72 @@
-CREATE DATABASE portfolio;
-USE portfolio;
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Czas generowania: 27 Kwi 2023, 13:41
+-- Wersja serwera: 10.4.19-MariaDB
+-- Wersja PHP: 8.0.7
 
-CREATE TABLE projects (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(50) NOT NULL,
-    image VARCHAR(100) NOT NULL,
-    description VARCHAR(255) NOT NULL
-);
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
 
-INSERT INTO `realizacje` (`id`, `tytul`, `opis`, `obrazek`) 
-VALUES (NULL, 'Realizacja nr 1', 'Przykładowy opis realizacji nr 1', 'img/release1.png');
 
-INSERT INTO `realizacje` (`id`, `tytul`, `opis`, `obrazek`) 
-VALUES (NULL, 'Realizacja nr 2', 'Przykładowy opis realizacji nr 2', 'img/release2.png');
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
 
-INSERT INTO `realizacje` (`id`, `tytul`, `opis`, `obrazek`) 
-VALUES (NULL, 'Realizacja nr 3', 'Przykładowy opis realizacji nr 3', 'img/release3.png');
+--
+-- Baza danych: `portfolio2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struktura tabeli dla tabeli `projects`
+--
+
+CREATE TABLE `projects` (
+  `id` int(6) UNSIGNED NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `description` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Zrzut danych tabeli `projects`
+--
+
+INSERT INTO `projects` (`id`, `title`, `image`, `description`) VALUES
+(1, 'Realizacja nr 1', 'img/release1.png', 'Przykładowy opis realizacji nr 1'),
+(2, 'Realizacja nr 2', 'img/release2.png', 'Przykładowy opis realizacji nr 2'),
+(3, 'Realizacja nr 3', 'img/release3.png', 'Przykładowy opis realizacji nr 3'),
+(4, 'Realizacja nr 4', 'img/release1.png', 'Przykładowy opis realizacji nr 4'),
+(5, 'Realizacja nr 5', 'img/release2.png', 'Przykładowy opis realizacji nr 5'),
+(6, 'Realizacja nr 6', 'img/release3.png', 'Przykładowy opis realizacji nr 6');
+
+--
+-- Indeksy dla zrzutów tabel
+--
+
+--
+-- Indeksy dla tabeli `projects`
+--
+ALTER TABLE `projects`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT dla zrzuconych tabel
+--
+
+--
+-- AUTO_INCREMENT dla tabeli `projects`
+--
+ALTER TABLE `projects`
+  MODIFY `id` int(6) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
